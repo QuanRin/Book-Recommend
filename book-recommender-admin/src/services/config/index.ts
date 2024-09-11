2ca1432fa6bca6499f7da979d1e2bf7c9a0add2e
Minh Quan <nguyenquan28322@gmail.com>
2024-09-11 20:49:15 +0700

    add config/services/src

---
+++ book-recommender-admin/src/services/config/index.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosResponse } from 'axios'
+
+
+const baseURL = `http://localhost:5757/v1`;
+
+const instance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+})
+
+const handleSuccessResponse = (response: AxiosResponse<any, any>) => {
+  return response
+}
+
+const handleErrorResponse = (error: any) => {
+  try {
+    return Promise.reject(error.response.data)
+  } catch (e) {
+    return Promise.reject({ message: 'Network Error' })
+  }
+}
+
+export const setHeaderConfigAxios = (token?: string) => {
+  if (token) {
+    instance.defaults.headers.common['Authorization'] = token ? 'Bearer ' + token : ''
+  } else {
+    delete instance.defaults.headers.common['Authorization']
+  }
+}
+
+instance.interceptors.response.use(handleSuccessResponse, handleErrorResponse)
+
+export default instance
